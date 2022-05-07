77f8bc752c8af5c2a44fb73206de7caac17480d3
iqblw <98456822+iqblw@users.noreply.github.com>
2022-05-08 01:09:47 +0700

    Update main.tf

--- Environment/Main/main.tf
+++ Environment/Main/main.tf
@@ -1,5 +1,8 @@
 module "vpc" {
   source                        = "../Module/VPC"
+  vpc_cidr_block                = "10.230.0.0/19"
+  public_subnet_cidr_block      = ["10.230.0.0/24", "10.230.1.0/24"]
+  eks_private_subnet_cidr_block = ["10.230.8.0/22", "10.230.12.0/22"]
 }
 
 module "eks" {
